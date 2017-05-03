d1ce7bbf5a9f78a9813f894dda987824a962945e
enomoha <mohamed.imran.noor.mohamed@ericsson.com>
2017-05-03 14:52:10 +0200

    complete task

--- collections/exercises/event-calendar/src/com/frotscher/coaching/collections/EventCalendarSolution.java
+++ collections/exercises/event-calendar/src/com/frotscher/coaching/collections/EventCalendarSolution.java
@@ -1,11 +1,12 @@
 package com.frotscher.coaching.collections;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
+
 
 
 public class EventCalendarSolution implements EventCalendar {
@@ -21,36 +22,46 @@ public class EventCalendarSolution implements EventCalendar {
 	@Override
 	public Collection<Event> getEventsBefore(LocalDateTime dateTime) {
 
-//	    eventCalendar.forEach((k,v)->{
-//	        if(eventCalendar.containsKey(dateTime)){
-//	            return eventCalendar.;
-//	        }
-//	    });
-        return null;
+        java.util.List<Event> eventsBefore = new ArrayList<>();
+
+        eventsBefore = eventCalendar.entrySet()
+                .stream()
+                .map(p -> new Event(p.getKey(), p.getValue()))
+                .filter(item -> item.getDateTime().isBefore(dateTime))
+                .collect(Collectors.toList());
+	    return eventsBefore;
 	}
+	
+	/**
+	 * Do the before and after and then proceed for logic on between should work fine
+	 */
 
 	@Override
 	public Collection<Event> getEventsAfter(LocalDateTime dateTime) {
 	    
-	    Set<Event> events = new HashSet<>();
-	    
-	    if(eventCalendar.containsKey(dateTime)){
-	        eventCalendar.forEach((k,v)->{
-	            //The stumbling part is the how we get EVENT from its characteristics.
+	    java.util.List<Event> eventsAfter = new ArrayList<>();
 
-            });
-	        
-	    }
-	    
-
-	    
-	    return java.util.Collections.emptyList();
+	    eventCalendar.forEach((k,v)->{
+	        Event myEvent = new Event(k, v);
+	        eventsAfter.add(myEvent);
+        });
+	    return eventsAfter;
 
 	}
 
 	@Override
 	public Collection<Event> getEventsBetween(LocalDateTime begin, LocalDateTime end) {
-		// TODO Auto-generated method stub
-	    return java.util.Collections.emptyList();
+	    
+	    java.util.List<Event> eventsBetween = new ArrayList<>();
+	    
+	    eventsBetween = eventCalendar.entrySet()
+	                                 .stream()
+	                                 .map(p -> new Event(p.getKey(), p.getValue()))
+	                                 .filter(item -> item.getDateTime().isAfter(begin) && item.getDateTime().isBefore(end))
+	                                 .collect(Collectors.toList());
+	    return eventsBetween;
 	}
+	
+	
+	
 }
