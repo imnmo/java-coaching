346f06bc4181dc326b1d350ec7754f8dc4c4972d
enomoha <mohamed.imran.noor.mohamed@ericsson.com>
2017-05-07 20:06:06 +0200

    Add solution for BI wo duplicates traking

--- collections/exercises/book-index/src/com/frotscher/coaching/collections/BookIndex.java
+++ collections/exercises/book-index/src/com/frotscher/coaching/collections/BookIndex.java
@@ -3,10 +3,11 @@ package com.frotscher.coaching.collections;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class BookIndex {
     
-    private Map<String,Integer> bookIndex = new LinkedHashMap<>();
+    private Map<String,Integer> bookIndex = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
     
     public void addReference(String term, int pageNum) {
         bookIndex.put(term, pageNum);
@@ -17,9 +18,18 @@ public class BookIndex {
             System.out.println("term:"+ k + "," + "pageNum" + v);
         });
     }
+    
+    public int getSize() {
+       return  bookIndex.size();
+    }
+    
+    public Map<String,Integer> getBookIndexMap(){
+        return bookIndex;
+    }
 
     @Override
     public String toString() {
         return Arrays.toString(bookIndex.entrySet().toArray()) ;
     }
 }
+
--- collections/exercises/book-index/src/com/frotscher/coaching/collections/BookIndexTest.java
+++ collections/exercises/book-index/src/com/frotscher/coaching/collections/BookIndexTest.java
@@ -3,30 +3,46 @@ package com.frotscher.coaching.collections;
 import static org.junit.Assert.*;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.junit.Test;
 
 public class BookIndexTest {
     
+    //create based on number of pages
+    int[] arrayForIndexing = new int[22];
+    int intItem;
+    
+    BookReader bookReader = new BookReader();
+    BookIndex bookIndex = new BookIndex();
+
     @Test
     public void test() {
-        
-        BookReader bookReader = new BookReader();
-        BookIndex bookIndex = new BookIndex();
+
         bookReader.getPageCount();
 
-       System.out.println(bookReader.getWordsOnPage(0));
-
-       //create based on number of pages
-//       int[] arrayForIndexing = new int[22];
-//       for (int intItem= 0;intItem <= arrayForIndexing.length-1;intItem++ ){
-//           List<String> wordOnPage = bookReader.getWordsOnPage(intItem);
-//           String word = wordOnPage.get(intItem);
-//           bookIndex.addReference(word, intItem);
-//       }
+       System.out.println(bookReader.getWordsOnPage(1));
+       
+       for (intItem= 0;intItem <= arrayForIndexing.length-1;intItem++ ){
+           List<String> wordOnPage = bookReader.getWordsOnPage(intItem);
+           wordOnPage.forEach(item -> bookIndex.addReference(item, intItem));
+       }
+       int size = bookIndex.getSize();
+       System.out.println(size);
        System.out.println(bookIndex.toString());
+   
+//       TreeMap<Integer, String> sortedKeys =
+//               new TreeMap<Integer, String>();
 
+//       bookIndex.getBookIndexMap().entrySet()
+//                                  .stream()
+//                                  .forEach(item -> item.getKey().compareTo());
+
+       System.out.println(bookIndex.toString());
     }
     
 }
